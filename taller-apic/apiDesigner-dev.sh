4401fcdc65e31a23c875715648ec3a8900b66765
jaimev <jaimev@vc-soft.com>
2025-01-22 12:29:00 -0500

    ajusto a versión local -lan

--- taller-apic/apiDesigner-dev.sh
+++ taller-apic/apiDesigner-dev.sh
@@ -1,5 +1,6 @@
 apic client-creds:set /Users/jaimevalencia/work/apic-taller/taller-apic/credentials-dev.json
-export DESIGNER_DEBUG=true
+#export DESIGNER_DEBUG=true
 export SKIP_IBMID_LOGIN=true
 export APIC_DESIGNER_CREDENTIALS=/Users/jaimevalencia/work/apic-taller/taller-apic/designer_credentials-dev.json 
+#export NODE_EXTRA_CA_CERTS=/Users/jaimevalencia/work/apic-taller/taller-apic/tallerDesarrollo/taller-apic-v1.0.0/api-manager-ui.cer
 open "/Applications/API Designer.app"
---
+++ taller-apic/apiDesigner-lan.sh
@@ -0,0 +1,6 @@
+apic client-creds:set /Users/jaimevalencia/work/apic-taller/apic10.0.8/credentials-lan.json
+#export DESIGNER_DEBUG=true
+export SKIP_IBMID_LOGIN=true
+export APIC_DESIGNER_CREDENTIALS=/Users/jaimevalencia/work/apic-taller/apic10.0.8/DesignerCredentials-lan.json 
+export NODE_EXTRA_CA_CERTS=/Users/jaimevalencia/work/apic-taller/apic10.0.8/apim-endpoint.pem
+open "/Applications/API Designer.app"
